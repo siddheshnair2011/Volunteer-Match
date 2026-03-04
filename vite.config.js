37a8fe7ce24a7f5b0d6567177f2121daa3fc5de7
siddheshnair2011 <siddhesh.nair2011@gmail.com>
2026-03-04 10:58:55 -0800

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,5 @@
+# Run this locally, then push to GitHub
+npm create vite@latest volunteer-finder -- --template react
+cd volunteer-finder
+# Replace src/App.jsx with your volunteer-agent.jsx code
+npm install
